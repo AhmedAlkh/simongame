f679335eaf21c3514a3da3734b4492866b5f364b
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-08-11 10:25:47 -0400

    created random number generator and attached it to colours array

---
+++ game.js
@@ -0,0 +1,10 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    var randomChosenColour = buttonColours[randomNumber];
+
+    gamePattern.push(randomChosenColour);
+};
