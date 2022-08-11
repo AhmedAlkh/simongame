d659b91274d03159ea49a4b184bc1a381913cb86
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-08-11 10:59:59 -0400

    game starts when a key is pressed, title updates accoring to level

--- game.js
+++ game.js
@@ -1,6 +1,16 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$(document).keydown(function() {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
 
 $(".btn").click(function() {
     var userChosenColour = $(this).attr("id");
@@ -12,6 +22,11 @@ $(".btn").click(function() {
 });
 
 function nextSequence() {
+
+    level++;
+
+    $("#level-title").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random() * 3) + 1;
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
