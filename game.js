3f9a7954bd6d615192c909f6b669b0fe12a84661
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-08-11 11:22:44 -0400

    added restart function to reset the game and play again.

--- game.js
+++ game.js
@@ -43,6 +43,8 @@ function checkAnswer(currentLevel) {
         },200);
 
         $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver();
     }
 };
 
@@ -78,4 +80,8 @@ function animatePress(currentColour) {
     },100);
 };
 
-
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+};
